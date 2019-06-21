d5f3eb8e412f074b2962ac0b24c73a1b9834be9d
Edgar <edgar@anotherfoxguy.com>
2019-06-21 10:46:40 +0200

    :poop: Update project

---
+++ Opdracht_C/Opdracht_C/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Opdracht_C
+{
+    class Client
+    {
+
+        private Socket sender;
+        private IPAddress ipAddress;
+        private IPEndPoint remoteEP;
+        private readonly byte[] bytes = new byte[1024];
+
+      
+        public bool CheckValidIpAddress(string ip)
+        {
+            if (ip != "")
+            {
+                //Check user input against regex (check if IP address is not empty).
+                Regex regex = new Regex("\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\\b");
+                Match match = regex.Match(ip);
+                return match.Success;
+            }
+            else return false;
+        }
+        public void SetConnection(string ip, int port)
+        {
+            ipAddress = IPAddress.Parse(ip);
+            remoteEP = new IPEndPoint(ipAddress, port);
+
+        }
+        public void OpenConnection()
+        {
+            sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(remoteEP);
+        }
+        public void CloseConnection()
+        {
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        public void SendData(string data)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            sender.Send(msg);
+        }
+        public string ReciveData(string dataToSend)
+        {
+            SendData(dataToSend);
+            int dataRecived = sender.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, dataRecived);
+        }
+    }
+}
---
+++ Opdracht_C/Opdracht_C/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Opdracht_C
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Client client = new Client();
+        Client laasteAr = new Client();
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (client.CheckValidIpAddress(Iptext.Text))
+            {
+                client.SetConnection(Iptext.Text, 8000);
+                if (e.Value)
+                {
+                    Test.Text = "True";
+                    Connecting("T");
+                }
+                else
+                {
+                    Test.Text = "flase";
+                    Connecting("F");
+                }
+            }
+            
+
+        }
+     
+        public void Connecting(string data)
+        {
+            
+            client.OpenConnection();
+            Test.Text = "test";
+            client.SendData(data);
+            client.CloseConnection();
+        }
+        
+
+
+    }
+}
